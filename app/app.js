e541040e7bb44019f03605f0bcc996d3c99f35ef
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-03-31 14:54:35 -0500

    develop commit

--- app/app.js
+++ app/app.js
@@ -17,13 +17,35 @@ class Profile {
     this.render();
   }
 
+  getData() {
+    fetch(`https://api.github.com/users/shonhartman`)
+      .then((response) => {
+      return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+    this.avatar = response.avatar;
+    this.username = response.username;
+    this.joined = response.joined;
+    this.followers = response.followers;
+    this.starred = response.starred;
+    this.following = response.following;
+    this.organizations = response.organizations;
+    this.repos = response.repos;
+
+    this.render();
+  });
+  }
+
   render() {
+
+    let 
     let h1 = document.querySelector = document.createElement("h1");
     h1.innerHTML = this.username;
 
 
-  let img = document.querySelector = document.createElement("img");
-  img.innerHTML = this.avatar;
+    let img = document.querySelector = document.createElement("img");
+    img.innerHTML = this.avatar;
 
   // let item =  document.querySelector("#textField").value;
   // let li = document.createElement("li");
@@ -33,11 +55,3 @@ class Profile {
 }
 
 }
-
-fetch (`https://api.github.com/users/shonhartman`)
-  .then((response) => {
-  return response.json();
-})
-.then((response) => {
-  console.log(response);
-});
